b616eb6cf5bfa60c1d68e0a3d2d40523804ee280
HAWOO <ttlhi10@gmail.com>
2024-09-02 00:07:33 +0900

    generate project

---
+++ src/main/kotlin/com/study/inflearn_msa/InflearnMsaApplication.kt
@@ -0,0 +1,11 @@
+package com.study.inflearn_msa
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class InflearnMsaApplication
+
+fun main(args: Array<String>) {
+	runApplication<InflearnMsaApplication>(*args)
+}
---
+++ src/test/kotlin/com/study/inflearn_msa/InflearnMsaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.study.inflearn_msa
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class InflearnMsaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
