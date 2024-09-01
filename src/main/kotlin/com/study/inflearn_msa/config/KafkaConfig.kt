5bab64e8c997236687d2d0ca56c3ff6711b2abf9
sungwoo <ttlhi10@gmail.com>
2024-09-02 00:47:21 +0900

    패키지 구조 정의

---
+++ src/main/kotlin/com/study/inflearn_msa/config/KafkaConfig.kt
@@ -0,0 +1,35 @@
+package com.study.inflearn_msa.config
+
+import org.apache.kafka.clients.admin.NewTopic
+import org.apache.kafka.clients.producer.ProducerConfig
+import org.apache.kafka.common.serialization.StringSerializer
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.kafka.core.DefaultKafkaProducerFactory
+import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.kafka.core.ProducerFactory
+import org.springframework.kafka.support.serializer.JsonSerializer
+
+@Configuration
+class KafkaConfig {
+
+    @Bean
+    fun producerFactory(): ProducerFactory<String, Any> {
+        val configProps = mapOf(
+            ProducerConfig.BOOTSTRAP_SERVERS_CONFIG to "localhost:9092",
+            ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG to StringSerializer::class.java,
+            ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG to JsonSerializer::class.java
+        )
+        return DefaultKafkaProducerFactory(configProps)
+    }
+
+    @Bean
+    fun kafkaTemplate(): KafkaTemplate<String, Any> {
+        return KafkaTemplate(producerFactory())
+    }
+
+    @Bean
+    fun topic(): NewTopic {
+        return NewTopic("example-topic", 1, 1.toShort())
+    }
+}
